52a61f2ff01cbac5248395ed8aaabb0de614008d
Yuyang Lao <yuyanglao@gmail.com>
2016-08-23 14:11:44 -0500

    add neighbor_charge.m

---
+++ neighbor_charge.m
@@ -0,0 +1,13 @@
+function neighbor_charge(startin)
+%This function calculate the average absolute charge value of four-island
+%vertices in the vicinity of a changed/unchanged three-island vertices
+switch nargin
+    case 1
+        start=startin;
+    otherwise
+        start=0;
+    end
+end
+
+end
+
